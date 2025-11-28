f062f3fa8ef9f90025a88c6c783d7be40d4a17f6
KushajSolanki <kushajsolanki0007@gmail.com>
2025-11-29 01:33:10 +0530

    Initial project + auth setup1

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,14 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// REGISTER ROUTES FIRST
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("Xeno Backend Running");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
-
-const authRoutes = require("./routes/auth.routes");
-
-// Routes
-app.use("/auth", authRoutes);
