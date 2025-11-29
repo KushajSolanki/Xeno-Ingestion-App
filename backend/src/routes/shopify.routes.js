ecf62513698fb0ee6d18d704e37cbf35ea6f9089
Kushaj Pavankumar Solanki <kushajsolanki0007@gmail.com>
2025-11-30 03:57:00 +0530

    Refactor shopify.routes.js for cleaner syntax

--- backend/src/routes/shopify.routes.js
+++ backend/src/routes/shopify.routes.js
@@ -1,18 +1,13 @@
+
 const express = require("express");
 const router = express.Router();
 const shopifyController = require("../controllers/shopify.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
-// START SHOPIFY INSTALL
 router.get("/install", shopifyController.install);
-
-// OAUTH CALLBACK
 router.get("/callback", shopifyController.callback);
 
-// STORE SUMMARY (Dashboard)
 router.get("/summary", authMiddleware, shopifyController.summary);
-
-// SYNC PRODUCTS (protected)
 router.post("/sync/products", authMiddleware, shopifyController.syncProducts);
 
 module.exports = router;
