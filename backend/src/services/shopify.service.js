544c0c4dc9fbeb61e9792b78d09aae5f6ed22a4a
Kushaj Pavankumar Solanki <kushajsolanki0007@gmail.com>
2025-11-29 21:54:02 +0530

    Refactor Shopify service to use generic API call

--- backend/src/services/shopify.service.js
+++ backend/src/services/shopify.service.js
@@ -1,53 +1,40 @@
 const axios = require("axios");
 
-async function fetchShopifyProducts(shopUrl, accessToken) {
-  const apiVersion = "2025-10"; // same as in your app config
-  const url = `https://${shopUrl}/admin/api/${apiVersion}/products.json?limit=250`;
+const API_VERSION = "2025-10";
 
+async function shopifyGet(path, shopUrl, accessToken, query = "") {
+  const url = `https://${shopUrl}/admin/api/${API_VERSION}/${path}.json${query}`;
   const res = await axios.get(url, {
     headers: {
       "X-Shopify-Access-Token": accessToken,
       "Content-Type": "application/json",
     },
   });
+  return res.data;
+}
 
-  return res.data.products || [];
+async function fetchShopifyProducts(shopUrl, accessToken) {
+  const data = await shopifyGet("products", shopUrl, accessToken, "?limit=250");
+  return data.products || [];
 }
 
 async function fetchShopifyCustomers(shopUrl, accessToken) {
-  const apiVersion = "2025-10";
-  const url = `https://${shopUrl}/admin/api/${apiVersion}/customers.json?limit=250`;
-
-  const res = await axios.get(url, {
-    headers: {
-      "X-Shopify-Access-Token": accessToken,
-      "Content-Type": "application/json",
-    },
-  });
-
-  return res.data.customers || [];
+  const data = await shopifyGet("customers", shopUrl, accessToken, "?limit=250");
+  return data.customers || [];
 }
 
 async function fetchShopifyOrders(shopUrl, accessToken) {
-  const apiVersion = "2025-10";
-  const url = `https://${shopUrl}/admin/api/${apiVersion}/orders.json?status=any&limit=250`;
-
-  const res = await axios.get(url, {
-    headers: {
-      "X-Shopify-Access-Token": accessToken,
-      "Content-Type": "application/json",
-    },
-  });
-
-  return res.data.orders || [];
+  const data = await shopifyGet(
+    "orders",
+    shopUrl,
+    accessToken,
+    "?status=any&limit=250"
+  );
+  return data.orders || [];
 }
-
-
 
 module.exports = {
   fetchShopifyProducts,
   fetchShopifyCustomers,
   fetchShopifyOrders
 };
-
-
