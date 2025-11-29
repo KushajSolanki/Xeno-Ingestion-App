121e454a4e287aff944c87f8f4a7f938ba4cd1f2
Kushaj Pavankumar Solanki <kushajsolanki0007@gmail.com>
2025-11-29 20:54:55 +0530

    Add fetchShopifyCustomers function
    Added a new function to fetch Shopify customers.

--- backend/src/services/shopify.service.js
+++ backend/src/services/shopify.service.js
@@ -14,6 +14,22 @@ async function fetchShopifyProducts(shopUrl, accessToken) {
   return res.data.products || [];
 }
 
+async function fetchShopifyCustomers(shopUrl, accessToken) {
+  const apiVersion = "2025-10";
+  const url = `https://${shopUrl}/admin/api/${apiVersion}/customers.json?limit=250`;
+
+  const res = await axios.get(url, {
+    headers: {
+      "X-Shopify-Access-Token": accessToken,
+      "Content-Type": "application/json",
+    },
+  });
+
+  return res.data.customers || [];
+}
+
+
 module.exports = {
   fetchShopifyProducts,
 };
+
